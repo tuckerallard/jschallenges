16fcb366ae91eb9cbe0aa19fbe59f57706534482
tuckerallard <tucker_allard@yahoo.com>
2016-09-14 10:53:16 -0400

    9/14/16 10:54am Challenge 2 alt complete

--- challenge_2_alt/challenge2_alt.js
+++ challenge_2_alt/challenge2_alt.js
@@ -1,35 +1,77 @@
-/**
- * Hockey is the game. Make it happen.
- *
- * If you want to turn it into some other sport, have at it.
- *
- * Anyway, I have given you a basic HTML structure for a 
- * BATTLE OF THE SPORTS GAME THING between these two rivals, and you
- * should make the page do what it needs to do, using your knowledge
- * of JS, HTML, CSS, and... sports.
- *
- * Here's what this 'game' should do:
- * 
- * 1. Clicking a "SHOOT" button attempt to score against the opposing team.
- *   - shots have a random chance of succeeding or failing
- *   - number of shots taken should increase every click on the "SHOOT" button
- *   - number of hits obviously only increases when the shot is successful
- * 
- * 2. Clicking the "RESET" button resets all the shot and score counters and
- * adds 1 to the number of resets
- *
- * 3. Any time a team shoots and scores change the background color of
- *    page to that teams color
- *
- * OPTIONAL STUFF:
- * - add logos of the two rivals below their name
- * - make the page just look better
- * - play a sound when someone clicks the "Shoot" button. You'll need to read about the <audio> element
- *   and how to use it in JS. You will also need to download a sound bite
- */
+$(function(){
+var shootButtonOne = $('#teamone-shoot');
+var shootButtonTwo = $('#teamtwo-shoot');
+var resetButton = $('#reset');
+var counter1 = 0;
+var counter2 = 0;
+var resetCounter = 0;
+var teamOneHits = 0;
+var teamTwoHits = 0;
 
- $(function(){
+//The function that determines the random roll for each try
+
+function getRandomIntInclusive() {
+ min = Math.ceil(0);
+ max = Math.floor(1);
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+//Links the team 1 shoot button to the random roll + counter
+
+shootButtonOne.click(function(){
+    counter1++;
+    $('#teamone-numshots').html(counter1);
+    var result = getRandomIntInclusive();
+    if (result ==1){
+        $('body').css("background-color","blue");
+        alert("GOAL!")
+        teamOneHits++;
+        $('#teamone-numhits').html(teamOneHits);
+}
+else {
+        $('body').css("background-color","white");
+        alert("Team 1 missed")
+}
+
+});
+
+//Links the team 2 shoot button to the random roll + counter
+
+shootButtonTwo.click(function(){
+    counter2++;
+    $('#teamtwo-numshots').html(counter2);
+    var result = getRandomIntInclusive();
+    if (result ==1){
+        $('body').css("background-color","red");
+        alert("GOAL!")
+        teamTwoHits++;
+        $('#teamtwo-numhits').html(teamTwoHits);
+}
+else {
+        $('body').css("background-color","white");
+        alert("Team 2 missed")
+}
+
+});
+
+//Links the reset button to the reset command which erases the counter
+
+resetButton.click(function(){
+    $('body').css("background-color","white");
+    resetCounter++;
+    counter1 = 0;
+    counter2 = 0;
+    teamOneHits = 0;
+    teamTwoHits = 0;
+    $('#num-resets').html(resetCounter)
+    $('#teamone-numshots').html(counter1);
+    $('#teamtwo-numshots').html(counter2);
+    $('#teamone-numhits').html(teamOneHits);
+    $('#teamtwo-numhits').html(teamTwoHits);
+});
 
 
 
- })
+
+
+})
